e8990fb99f6dfa7115bd69b70b35a6c0ba89a307
dxlin <danielx.lin@gmail.com>
2015-05-09 01:27:32 -0400

    adding estimote library

--- mindfulhacks/app/src/main/java/wearhacks/mindfulhacks/MainActivity.java
+++ mindfulhacks/app/src/main/java/wearhacks/mindfulhacks/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.interaxon.libmuse.Accelerometer;
+import com.estimote.sdk.Beacon;
 
 public class MainActivity extends ActionBarActivity {
 
