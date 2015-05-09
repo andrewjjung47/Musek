7f3d769f7bec2c15dd3f5da28e41d543e7e87054
dxlin <danielx.lin@gmail.com>
2015-05-09 14:56:31 -0400

    estimote now detects range for specified estimotes

--- mindfulhacks/app/src/main/java/wearhacks/mindfulhacks/MainActivity.java
+++ mindfulhacks/app/src/main/java/wearhacks/mindfulhacks/MainActivity.java
@@ -15,6 +15,7 @@ import java.io.File;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import android.app.Activity;
 import android.os.Environment;
@@ -24,6 +25,9 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+import com.estimote.sdk.Utils;
 import com.interaxon.libmuse.Accelerometer;
 import com.interaxon.libmuse.ConnectionState;
 import com.interaxon.libmuse.Eeg;
@@ -45,9 +49,10 @@ import com.estimote.sdk.Beacon;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-//    private BeaconManager beaconManager;
-//    private ArrayList<Beacon> beacons;
-//    private ArrayList<Region> regions;
+    private BeaconManager beaconManager;
+    private ArrayList<Beacon> beacons;
+
+    private static final Region ALL_ESTIMOTE_BEACONS_REGION = new Region("rid", null, null, null);
 
     MediaPlayer mediaPlayer;
 
@@ -273,39 +278,36 @@ public class MainActivity extends Activity implements OnClickListener {
         });
 
         // Initializing beacon, region, and manager to connect to estimote
-//        beacons = new ArrayList<Beacon>();
-//        beacons.add(new Beacon( "b9407f30-f5f8-466e-aff9-25556b57fe6d",
-//                                "estimote",
-//                                "E5:3D:D0:63:FD:88",
-//                                64904, 53347,
-//                                -74, -62));
-//
-//        beacons.add(new Beacon("b9407f30-f5f8-466e-aff9-25556b57fe6d",
-//                               "estimote",
-//                               "E5:3D:D0:63:FD:88",
-//                               10470, 33680,
-//                               -74, -41));
-//
-//        regions = new ArrayList<Region>();
-//        regions.add(new Region("regionid0", beacons.get(0).getProximityUUID(), beacons.get(0).getMajor(), beacons.get(0).getMinor()));
-//        //regions.add(new Region("regionid1", beacons.get(1).getProximityUUID(), beacons.get(1).getMajor(), beacons.get(1).getMinor()));
-//
-//        //regions.add(new Region("regionid", beacons.get(0).getProximityUUID()));
-//
-//        beaconManager = new BeaconManager(this);
-//
-//        // Default values are 5s of scanning and 25s of waiting time to save CPU cycles.
-//        // In order for this demo to be more responsive and immediate we lower down those values.
-//        beaconManager.setBackgroundScanPeriod(TimeUnit.SECONDS.toMillis(1), 0);
-//
+        beacons = new ArrayList<Beacon>();
+        beacons.add(new Beacon( "b9407f30-f5f8-466e-aff9-25556b57fe6d",
+                                "estimote",
+                                "E5:3D:D0:63:FD:88",
+                                64904, 53347,
+                                -74, -62));
+
+        beacons.add(new Beacon("b9407f30-f5f8-466e-aff9-25556b57fe6d",
+                               "estimote",
+                               "E5:3D:D0:63:FD:88",
+                               10470, 33680,
+                               -74, -41));
+
+        beaconManager = new BeaconManager(this);
+
+        // Default values are 5s of scanning and 25s of waiting time to save CPU cycles.
+        // In order for this demo to be more responsive and immediate we lower down those values.
+        beaconManager.setBackgroundScanPeriod(TimeUnit.SECONDS.toMillis(1), 0);
+
+        final ArrayList<Beacon> beaconList = new ArrayList<Beacon>(beacons);
+
 //        // Choose what to do when we enter/leave a region with a beacon
 //        beaconManager.setMonitoringListener(new BeaconManager.MonitoringListener() {
 //            @Override
 //            public void onEnteredRegion(Region region, List<Beacon> beacons) {
 //                for (Beacon rangedBeacon : beacons) {
-//                    if (rangedBeacon.getMacAddress().equals(beacons.get(0).getMacAddress())) {
+//                    Log.v("dbg", rangedBeacon.getMajor() + "");
+//                    if (rangedBeacon.getMajor() == beaconList.get(0).getMajor()) {
 //                        Log.v("dbg", "In region of beacon 0");
-//                    } else if (rangedBeacon.getMacAddress().equals(beacons.get(0).getMacAddress())) {
+//                    } else if (rangedBeacon.getMajor() == beaconList.get(1).getMajor()) {
 //                        Log.v("dbg", "In region of beacon 1");
 //                    }
 //                }
@@ -317,6 +319,30 @@ public class MainActivity extends Activity implements OnClickListener {
 //                Log.v("dbg", "Left region");
 //            }
 //        });
+
+        // Configure BeaconManager.
+        beaconManager = new BeaconManager(this);
+        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
+            @Override
+            public void onBeaconsDiscovered(Region region, final List<Beacon> beacons) {
+                for (Beacon rangedBeacon : beacons) {
+                    Log.v("dbg", rangedBeacon.getMajor() + "");
+                    if (rangedBeacon.getMajor() == beaconList.get(0).getMajor()) {
+                        double distance = Math.min(Utils.computeAccuracy(rangedBeacon), 6.0);
+                        Log.v("dbg", distance + "");
+                        TextView tv = (TextView) findViewById(R.id.TV1);
+                        tv.setText(String.valueOf(distance));
+                    } else if (rangedBeacon.getMajor() == beaconList.get(1).getMajor()) {
+                        Log.v("dbg", "In region of beacon 1");
+                        double distance = Math.min(Utils.computeAccuracy(rangedBeacon), 6.0);
+                        //updateDistanceView(foundBeacon);
+                        Log.v("dbg", distance + "");
+                        TextView tv = (TextView) findViewById(R.id.TV2);
+                        tv.setText(String.valueOf(distance));
+                    }
+                }
+            }
+        });
 
 //        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
 //            @Override
@@ -483,21 +509,16 @@ public class MainActivity extends Activity implements OnClickListener {
     private void connect() {
         Log.v("dbg", "Connect");
 
-        // Connect to estimote
-//        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
-//            @Override
-//            public void onServiceReady() {
-//                try {
-////                    beaconManager.startRanging(regions.get(0));
-////                    beaconManager.startRanging(regions.get(1));
-//                    beaconManager.startMonitoring(regions.get(0));
-//                } catch (RemoteException e) {
-//                    Toast.makeText(MainActivity.this, "Cannot start ranging, something terrible happened",
-//                            Toast.LENGTH_LONG).show();
-//                    Log.e("dbg", "Cannot start ranging", e);
-//                }
-//            }
-//        });
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                try {
+                    beaconManager.startRanging(ALL_ESTIMOTE_BEACONS_REGION);
+                } catch (RemoteException e) {
+                    Log.e("Connect", "Cannot start ranging", e);
+                }
+            }
+        });
 
     }
 
