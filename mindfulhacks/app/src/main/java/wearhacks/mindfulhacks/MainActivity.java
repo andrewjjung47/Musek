ae07db5e13a45efc78c8e280d8e0b898353d43c2
Andrew Jongho Jung <andrewjjung47@gmail.com>
2015-05-09 21:01:08 -0400

    Refactor Muse into another class.

--- mindfulhacks/app/src/main/java/wearhacks/mindfulhacks/MainActivity.java
+++ mindfulhacks/app/src/main/java/wearhacks/mindfulhacks/MainActivity.java
@@ -50,6 +50,8 @@ import com.interaxon.libmuse.MuseManager;
 import com.interaxon.libmuse.MusePreset;
 import com.interaxon.libmuse.MuseVersion;
 
+import wearhacks.mindfulhacks.MindfulMuse;
+
 import com.estimote.sdk.Beacon;
 
 public class MainActivity extends Activity implements OnClickListener {
@@ -57,224 +59,9 @@ public class MainActivity extends Activity implements OnClickListener {
     MediaPlayer mediaPlayer;
 
     private EstimoteDetector estimoteDetector;
-
-
-    /**
-     * Connection listener updates UI with new connection status and logs it.
-     */
-    class ConnectionListener extends MuseConnectionListener {
-
-        final WeakReference<Activity> activityRef;
-
-        ConnectionListener(final WeakReference<Activity> activityRef) {
-            this.activityRef = activityRef;
-        }
-
-        @Override
-        public void receiveMuseConnectionPacket(MuseConnectionPacket p) {
-            final ConnectionState current = p.getCurrentConnectionState();
-            final String status = p.getPreviousConnectionState().toString() +
-                    " -> " + current;
-            final String full = "Muse " + p.getSource().getMacAddress() +
-                    " " + status;
-            Log.i("Muse Headband", full);
-            Activity activity = activityRef.get();
-            // UI thread is used here only because we need to update
-            // TextView values. You don't have to use another thread, unless
-            // you want to run disconnect() or connect() from connection packet
-            // handler. In this case creating another thread is required.
-            if (activity != null) {
-                activity.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        TextView statusText =
-                                (TextView) findViewById(R.id.con_status);
-                        statusText.setText(status);
-                        if (current == ConnectionState.CONNECTED) {
-                            MuseVersion museVersion = muse.getMuseVersion();
-                            String version = museVersion.getFirmwareType() +
-                                    " - " + museVersion.getFirmwareVersion() +
-                                    " - " + Integer.toString(
-                                    museVersion.getProtocolVersion());
-                        }
-                    }
-                });
-            }
-        }
-    }
-
-    class DataListener extends MuseDataListener {
-
-        final WeakReference<Activity> activityRef;
-        private MuseFileWriter fileWriter;
-
-        DataListener(final WeakReference<Activity> activityRef) {
-            this.activityRef = activityRef;
-        }
-
-        @Override
-        public void receiveMuseDataPacket(MuseDataPacket p) {
-            switch (p.getPacketType()) {
-                case ALPHA_RELATIVE:
-                    updateAlphaRelative(p.getValues());
-                    break;
-                case BATTERY:
-                    fileWriter.addDataPacket(1, p);
-                    // It's library client responsibility to flush the buffer,
-                    // otherwise you may get memory overflow.
-                    if (fileWriter.getBufferedMessagesSize() > 8096)
-                        fileWriter.flush();
-                    break;
-                case CONCENTRATION:
-                    updateConcentration(p.getValues());
-                    break;
-                case MELLOW:
-                    updateMellow(p.getValues());
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        @Override
-        public void receiveMuseArtifactPacket(MuseArtifactPacket p) {
-            if (p.getHeadbandOn() && p.getBlink()) {
-                Log.i("Artifacts", "blink");
-            }
-        }
-
-        private void updateAlphaRelative(final ArrayList<Double> data) {
-            Activity activity = activityRef.get();
-            if (activity != null) {
-                activity.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        TextView elem1 = (TextView) findViewById(R.id.elem1);
-                        TextView elem2 = (TextView) findViewById(R.id.elem2);
-                        TextView elem3 = (TextView) findViewById(R.id.elem3);
-                        TextView elem4 = (TextView) findViewById(R.id.elem4);
-                        elem1.setText(String.format(
-                                "%6.2f", data.get(Eeg.TP9.ordinal())));
-                        elem2.setText(String.format(
-                                "%6.2f", data.get(Eeg.FP1.ordinal())));
-                        elem3.setText(String.format(
-                                "%6.2f", data.get(Eeg.FP2.ordinal())));
-                        elem4.setText(String.format(
-                                "%6.2f", data.get(Eeg.TP10.ordinal())));
-                    }
-                });
-            }
-        }
-
-        private void updateConcentration(final ArrayList<Double> data) {
-            Activity activity = activityRef.get();
-            if (activity != null) {
-                activity.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        TextView conc = (TextView) findViewById(R.id.conc);
-                        conc.setText(String.format(
-                                "%6.2f", data.get(0)));
-                    }
-                });
-            }
-
-            updateUserState(data, "conc");
-        }
-
-        private void updateMellow(final ArrayList<Double> data) {
-            Activity activity = activityRef.get();
-            if (activity != null) {
-                activity.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        TextView mellow = (TextView) findViewById(R.id.mellow);
-                        mellow.setText(String.format(
-                                "%6.2f", data.get(0)));
-                    }
-                });
-            }
-
-            updateUserState(data, "mellow");
-        }
-
-        private void updateUserState(final ArrayList<Double> data, String dataType) {
-            if (dataType == "mellow") {
-                mellow = data.get(0);
-            }
-            else if (dataType == "conc"){
-                conc = data.get(0);
-            }
-
-            if (mellow < 0.5 && conc < 0.5) {
-                userState = UserState.LOLO;
-            }
-            else if (mellow < 0.5 & conc >= 0.5) {
-                userState = UserState.LOHI;
-            }
-            else if (mellow >= 0.5 & conc < 0.5) {
-                userState = UserState.HILO;
-            }
-            else if (mellow >= 0.5 & conc >= 0.5) {
-                userState = UserState.HIHI;
-            }
-
-            Activity activity = activityRef.get();
-            if (activity != null) {
-                activity.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        TextView state = (TextView) findViewById(R.id.userState);
-                        state.setText(userState.toString());
-                    }
-                });
-            }
-        }
-
-        public void setFileWriter(MuseFileWriter fileWriter) {
-            this.fileWriter  = fileWriter;
-        }
-    }
-
-    private Muse muse = null;
-    private ConnectionListener connectionListener = null;
-    private DataListener dataListener = null;
-    private boolean dataTransmission = true;
-    private MuseFileWriter fileWriter = null;
-    private UserState userState = UserState.LOLO;
-    private Double mellow = 0.0;
-    private Double conc = 0.0;
-
-    public enum UserState {
-        LOLO {
-            public String toString() {
-                return "LOLO";
-            }
-        },
-        LOHI {
-            public String toString() {
-                return "LOHI";
-            }
-        },
-        HILO {
-            public String toString() {
-                return "HILO";
-            }
-        },
-        HIHI {
-            public String toString() {
-                return "HIHI";
-            }
-        }
-    }
+    private MindfulMuse mindfulMuse;
 
     public MainActivity() {
-        // Create listeners and pass reference to activity to them
-        WeakReference<Activity> weakActivity =
-                new WeakReference<Activity>(this);
-
-        connectionListener = new ConnectionListener(weakActivity);
-        dataListener = new DataListener(weakActivity);
     }
 
     @Override
@@ -287,12 +74,7 @@ public class MainActivity extends Activity implements OnClickListener {
         connectButton.setOnClickListener(this);
         Button disconnectButton = (Button) findViewById(R.id.disconnect);
         disconnectButton.setOnClickListener(this);
-        fileWriter = MuseFileWriterFactory.getMuseFileWriter(new File(
-                getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS),
-                "testlibmusefile.muse"));
-        Log.i("Muse Headband", "libmuse version=" + LibMuseVersion.SDK_VERSION);
-        fileWriter.addAnnotationString(1, "MainActivity onCreate");
-        dataListener.setFileWriter(fileWriter);
+
 
         initialize();
     }
@@ -337,6 +119,10 @@ public class MainActivity extends Activity implements OnClickListener {
         // Automatically plays local file uptownfunk in /res/raw
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.uptownfunk);
 //        mediaPlayer.start(); // no need to call prepare(); create() does that for you
+
+        mindfulMuse = new MindfulMuse(this, new File(
+                getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS),
+                "testlibmusefile.muse"));
     }
 
     @Override
@@ -352,89 +138,9 @@ public class MainActivity extends Activity implements OnClickListener {
     @Override
     public void onClick(View v) {
         Spinner musesSpinner = (Spinner) findViewById(R.id.muses_spinner);
-        if (v.getId() == R.id.refresh) {
-            MuseManager.refreshPairedMuses();
-            List<Muse> pairedMuses = MuseManager.getPairedMuses();
-            List<String> spinnerItems = new ArrayList<String>();
-            for (Muse m: pairedMuses) {
-                String dev_id = m.getName() + "-" + m.getMacAddress();
-                Log.i("Muse Headband", dev_id);
-                spinnerItems.add(dev_id);
-            }
-            ArrayAdapter<String> adapterArray = new ArrayAdapter<String> (
-                    this, android.R.layout.simple_spinner_item, spinnerItems);
-            musesSpinner.setAdapter(adapterArray);
-        }
-        else if (v.getId() == R.id.connect) {
-            List<Muse> pairedMuses = MuseManager.getPairedMuses();
-            if (pairedMuses.size() < 1 ||
-                    musesSpinner.getAdapter().getCount() < 1) {
-                Log.w("Muse Headband", "There is nothing to connect to");
-            }
-            else {
-                muse = pairedMuses.get(musesSpinner.getSelectedItemPosition());
-                ConnectionState state = muse.getConnectionState();
-                if (state == ConnectionState.CONNECTED ||
-                        state == ConnectionState.CONNECTING) {
-                    Log.w("Muse Headband", "doesn't make sense to connect second time to the same muse");
-                    return;
-                }
-                configure_library();
-                fileWriter.open();
-                fileWriter.addAnnotationString(1, "Connect clicked");
-                /**
-                 * In most cases libmuse native library takes care about
-                 * exceptions and recovery mechanism, but native code still
-                 * may throw in some unexpected situations (like bad bluetooth
-                 * connection). Print all exceptions here.
-                 */
-                try {
-                    muse.runAsynchronously();
-                } catch (Exception e) {
-                    Log.e("Muse Headband", e.toString());
-                }
-            }
-        }
-        else if (v.getId() == R.id.disconnect) {
-            if (muse != null) {
-                /**
-                 * true flag will force libmuse to unregister all listeners,
-                 * BUT AFTER disconnecting and sending disconnection event.
-                 * If you don't want to receive disconnection event (for ex.
-                 * you call disconnect when application is closed), then
-                 * unregister listeners first and then call disconnect:
-                 * muse.unregisterAllListeners();
-                 * muse.disconnect(false);
-                 */
-                muse.disconnect(true);
-                fileWriter.addAnnotationString(1, "Disconnect clicked");
-                fileWriter.flush();
-                fileWriter.close();
-            }
-        }
+        mindfulMuse.processInput(v, musesSpinner, this);
     }
 
-    private void configure_library() {
-        muse.registerConnectionListener(connectionListener);
-        muse.registerDataListener(dataListener,
-                MuseDataPacketType.ACCELEROMETER);
-        muse.registerDataListener(dataListener,
-                MuseDataPacketType.EEG);
-        muse.registerDataListener(dataListener,
-                MuseDataPacketType.ALPHA_RELATIVE);
-        muse.registerDataListener(dataListener,
-                MuseDataPacketType.ARTIFACTS);
-        muse.registerDataListener(dataListener,
-                MuseDataPacketType.BATTERY);
-        muse.registerDataListener(dataListener,
-                MuseDataPacketType.CONCENTRATION);
-        muse.registerDataListener(dataListener,
-                MuseDataPacketType.MELLOW);
-        muse.setPreset(MusePreset.PRESET_14);
-        muse.enableDataTransmission(dataTransmission);
-    }
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
